8c759db22f6c76f48f368b25ea98ce59c70f589d
fireganqQ <korayeren2006@gmail.com>
2020-12-15 02:04:49 +0300

    first commit

---
+++ tg_bot/__init__.py
@@ -0,0 +1,55 @@
+import logging, os, sys
+
+import telegram.ext as tg
+
+# günlük kaydı etkinleştir
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(massage)s",
+    level=logging.INFO)
+
+LOGGER = logging.getLogger(__name__)
+
+# sürüm <3.6 ise, botu durdurun.
+
+if sys.version_info[0] < 3 or sys.version_info[1] < 6:
+    LOGGER.error("En az 3.6 python sürümüne sahip olmalısınız! Birden çok özellik buna bağlıdır. Bot bırakma.")
+    quit(1)
+
+ENV = bool(os.environ.get("ENV", False))
+
+if ENV:
+    TOKEN = os.environ.get("TOKEN", None)
+    try:
+        OWNER_ID = int(os.environ.get("OWNER_ID", None))
+    except ValueError:
+        raise Exception("OWNER_ID env değişkeniniz geçerli bir tam sayı değil")
+
+    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", None)
+    WEBHOOK = bool(os.environ.get("WEBHOOK", False))
+    URL = os.environ.get("URL", "") # Jeton içermez
+    PORT = int(os.environ.get("PORT", 5000))
+    ALLOW_EXCL = os.environ.get("ALLOW_EXCL", False)
+else:
+    from tg_bot.config import Development as Config
+    TOKEN = Config.API_KEY
+    try:
+        OWNER_ID = int(Config.OWNER_ID)
+    except ValueError:
+        raise Exception("OWNER_ID değişkeniniz geçerli bir tam sayı değil")
+
+    OWNER_USERNAME = Config.OWNER_USERNAME
+    WEBHOOK = Config.WEBHOOK
+    URL = Config.URL
+    PORT = Config.PORT
+    ALLOW_EXCL = Config.ALLOW_EXCL
+updater = tg.Updater(TOKEN, workers=WORKERS)
+dispatcher = updater.dispatcher
+
+# Önceki tüm değişkenlerin ayarlandığından emin olmak için sonuna yükleyin
+from tg_bot.modules.helper_funcs.handlers import CustomCommandHandler, CustomRegexHandler
+
+# normal ifade işleyicisinin fazladan anahtarlar alabileceğinden emin olun
+tg.RegexHandler = CustomRegexHandler
+
+if ALLOW_EXCL:
+    tg.CommandHandler = CustomCommandHandler
---
+++ tg_bot/__main__.py
@@ -0,0 +1,43 @@
+import importlib, re
+from typing import Optional, List
+
+from telegram import Message, Chat, Update, Bot, User
+from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.error import Unauthorized, BadRequest, TimeOut, NetworkError, ChatMigrated, TelegramError
+from telegram.ext import CommandHandler, Filters, MessageHandler, CallbackQueryHandler
+from telegram.ext.dispatcher import run_async, DispatcherHandlerStop
+from telegram.utils.helpers import escape_markdown
+
+from tg_bot import dispatcher, updater, TOKEN, WEBHOOK, OWNER_ID, PORT, URL, LOGGER, ALLOW_EXCL
+
+# modülleri dinamik olarak yüklemek için gerekli
+# NOT: Modül sırası garanti edilmez, bunu yapılandırma dosyasında belirtin!
+from tg_bot.modules import ALL_MODULES
+from tg_bot.modules.helper_funcs.chat_status import is_user_admin
+from tg_bot.modules.helpers_funcs.misc import paginate_modules
+#========================KURULUM BİTİM===============================
+
+PM_START_TEXT = """
+Merhaba {} Benim Adım {}! Beni Nasıl Kullanacağınıza Dair Soruların Varsa /help \
+Komutunu Kullan!
+
+Herhangi Bir Sorunuz Varsa Yada Hata Bildirmek İcin Lütden [Sahibim'e](t.me/fireganqq) Bildirmekden Cekinme!
+Ayrıca Yeni Özellikler, Kesinti Süresi Vb. için [Duyuru Kanalıma](t.me/fireqanQBotlari) Baka Bilirsin!!
+"""
+
+HELP_TEXT1 = """
+Selam! Benim ismim *{}*.
+
+*Komutlar:*
+ - /start: `Botu Başlatır`
+ - /help: `Botu Nasıl Kullanacağınıza Dair Bilgi Verir`
+ - /ss: `Sectiğiniz Bir Tane Doğruluk Veya Cesaret Sorusu Sorar`
+ """
+
+@run_async
+def start(bot: Bot, update: Update):
+    update.effective_message.reply_text(PM_START_TEXT)
+
+@run_async
+def help(bot: Bot, update: Update):
+    update.effective_message.reply_text(HELP_TEXT1)
